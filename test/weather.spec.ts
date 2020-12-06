be724ffc61ee0d942516991d849fabf5a5992433
Stony Chen <stonychen@hotmail.com>
2020-12-06 15:43:28 +0800

    Update weather.spec.ts

--- test/weather.spec.ts
+++ test/weather.spec.ts
@@ -3,18 +3,19 @@ import should from 'should'
 import WeatherService from "./weather-service"
 
 const weatherService = new WeatherService()
-describe('Weather request', function () {
 
-  it('request getRegionCountry', function () {
-    weatherService.getRegionCountry().then(res => {
-      should(res.data.ArrayOfString.length).be.greaterThan(0)
-    })
+describe('Weather request', () => {
+
+  it('request getRegionCountry', async () => {
+
+    const res = await weatherService.getRegionCountry()
+    should(res.data.ArrayOfString.length).be.greaterThan(0)
   })
 
-  it('Request getRegionDataset', function () {
-    weatherService.getRegionDataset().then(res => {
-      should(!!res.data.DataSet).be.equal(true)
-    })
+  it('Request getRegionDataset', async function () {
+
+    const res = await weatherService.getRegionDataset()
+    should(!!res.data.DataSet).be.equal(true)
   })
 
 
